f01a5e4f3625050745e195e6cb90843635d6cede
mabelpertot <mabelgem77@gmail.com>
2024-10-10 07:34:17 +0000

    Punto_D_OK

--- RTA_SCRIPT_Examen_20241008/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_D.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo mkdir -p Estructura_Asimetrica/{{correo,cliente}/cartas_{1..100},correo/carteros_{1..10}}
+
+sudo mkdir -p Estructura_Asimetrica/{{correo,cliente}/cartas_{1..100},correo/carteros{1..10}}
+
+
+
 
 
 
